993a3cb826d629f9415d713fff562e52d80a4290
algo7 <11154774+algo7@users.noreply.github.com>
2022-01-09 01:48:30 +0100

    update clear cache script + add it to pkg json

--- clear_cache.sh
+++ clear_cache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -rf ~/Library/Containers/com.microsoft.Excel/Data/Library/Application\ Support/Microsoft/Office/16.0/Wef/
 rm -rf ~/Library/Containers/com.microsoft.Excel/Data/Library/Caches
+echo "Excel cache cleared"
