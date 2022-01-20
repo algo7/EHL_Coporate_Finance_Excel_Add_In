9e7f6f91a1ae8672c60d1f27c23ce16215742038
algo7 <11154774+algo7@users.noreply.github.com>
2022-01-20 23:48:10 +0100

    comment out cs related functions

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,198 +1,198 @@
-﻿/**
- * DDM 2 Stage Growth
- * @customfunction
- * @param {number} growth_rate_1 Growth rate in the beginning
- * @param {number} growth_rate_2 Growth rate in the end till infinity
- * @param {number} required_return Required return
- * @param {number} period Period in years
- * @param {number} starting_devidend starting devidend
- * @returns {number} Evaluation
- */
-function DDM_2STAGE(growth_rate_1, growth_rate_2, required_return, period, starting_devidend) {
-  const a = [starting_devidend];
-  const b = [];
+﻿// /**
+//  * DDM 2 Stage Growth
+//  * @customfunction
+//  * @param {number} growth_rate_1 Growth rate in the beginning
+//  * @param {number} growth_rate_2 Growth rate in the end till infinity
+//  * @param {number} required_return Required return
+//  * @param {number} period Period in years
+//  * @param {number} starting_devidend starting devidend
+//  * @returns {number} Evaluation
+//  */
+// function DDM_2STAGE(growth_rate_1, growth_rate_2, required_return, period, starting_devidend) {
+//   const a = [starting_devidend];
+//   const b = [];
 
-  for (let index = 0; index < period - 1; index++) {
-    a[index + 1] = a[index] * (1 + growth_rate_1);
-  }
+//   for (let index = 0; index < period - 1; index++) {
+//     a[index + 1] = a[index] * (1 + growth_rate_1);
+//   }
 
-  for (let index = 1; index < a.length; index++) {
-    b.push(a[index - 1] / (1 + required_return) ** index);
-  }
-  b.push(a[a.length - 1] / (1 + required_return) ** period);
+//   for (let index = 1; index < a.length; index++) {
+//     b.push(a[index - 1] / (1 + required_return) ** index);
+//   }
+//   b.push(a[a.length - 1] / (1 + required_return) ** period);
 
-  const xxx = a[a.length - 1] * (1 + growth_rate_2);
-  const yyy = (1 + required_return) ** period * (required_return - growth_rate_2);
+//   const xxx = a[a.length - 1] * (1 + growth_rate_2);
+//   const yyy = (1 + required_return) ** period * (required_return - growth_rate_2);
 
-  const sum = b.reduce(function (x, y) {
-    return x + y;
-  }, 0);
+//   const sum = b.reduce(function (x, y) {
+//     return x + y;
+//   }, 0);
 
-  return sum + xxx / yyy;
-}
+//   return sum + xxx / yyy;
+// }
 
-/**
- * DDM Constant Growth
- * @customfunction
- * @param {number} x
- * @param {number} required_return Required Return
- * @param {number} growth_rate Growth Rate
- * @returns {number} Evaluation
- */
-function DDM_CONSTANT_GROWTH(x, growth_rate, required_return) {
-  return (x * (1 + growth_rate)) / (required_return - growth_rate);
-}
+// /**
+//  * DDM Constant Growth
+//  * @customfunction
+//  * @param {number} x
+//  * @param {number} required_return Required Return
+//  * @param {number} growth_rate Growth Rate
+//  * @returns {number} Evaluation
+//  */
+// function DDM_CONSTANT_GROWTH(x, growth_rate, required_return) {
+//   return (x * (1 + growth_rate)) / (required_return - growth_rate);
+// }
 
-/**
- * DDM Constant Growth
- * @customfunction
- * @param {number} x
- * @param {number} required_return Required Return
- * @returns {number} Evaluation
- */
-function DDM_ZERO_GROWTH(x, required_return) {
-  return x / required_return;
-}
+// /**
+//  * DDM Constant Growth
+//  * @customfunction
+//  * @param {number} x
+//  * @param {number} required_return Required Return
+//  * @returns {number} Evaluation
+//  */
+// function DDM_ZERO_GROWTH(x, required_return) {
+//   return x / required_return;
+// }
 
-/**
- * Sharpe Ratio
- * @customfunction
- * @param {number} rt Return
- * @param {number} rf Risk Free Rate
- * @param {number} vol Volatility
- * @returns {number} Sharpe Ratio
- */
-function SHARP_RATIO(rt, rf, vol) {
-  return (rt - rf) / vol;
-}
+// /**
+//  * Sharpe Ratio
+//  * @customfunction
+//  * @param {number} rt Return
+//  * @param {number} rf Risk Free Rate
+//  * @param {number} vol Volatility
+//  * @returns {number} Sharpe Ratio
+//  */
+// function SHARP_RATIO(rt, rf, vol) {
+//   return (rt - rf) / vol;
+// }
 
-/**
- * CAPM Expected Return
- * @customfunction
- * @param {number} rf Risk Free Rate
- * @param {number} beta Beta
- * @param {number} rp Risk Premium
- * @returns {number} Expected Return
- */
-function CAPM_EXPECTED_RETURN(rf, beta, rp) {
-  return rf + beta * rp;
-}
+// /**
+//  * CAPM Expected Return
+//  * @customfunction
+//  * @param {number} rf Risk Free Rate
+//  * @param {number} beta Beta
+//  * @param {number} rp Risk Premium
+//  * @returns {number} Expected Return
+//  */
+// function CAPM_EXPECTED_RETURN(rf, beta, rp) {
+//   return rf + beta * rp;
+// }
 
-/**
- * Volatility Annualization - Daily
- * @customfunction
- * @param {number} stdev Standard Deviation of the returns
- * @returns {number} Annualized Risk
- */
-function RISK_ANNUALIZED_DAILY(stdev) {
-  return stdev * Math.sqrt(252);
-}
+// /**
+//  * Volatility Annualization - Daily
+//  * @customfunction
+//  * @param {number} stdev Standard Deviation of the returns
+//  * @returns {number} Annualized Risk
+//  */
+// function RISK_ANNUALIZED_DAILY(stdev) {
+//   return stdev * Math.sqrt(252);
+// }
 
-/**
- * Volatility Annualization - Weekly
- * @customfunction
- * @param {number} stdev Standard Deviation of the returns
- * @returns {number} Annualized Risk
- */
-function RISK_ANNUALIZED_WEEKLY(stdev) {
-  return stdev * Math.sqrt(52);
-}
+// /**
+//  * Volatility Annualization - Weekly
+//  * @customfunction
+//  * @param {number} stdev Standard Deviation of the returns
+//  * @returns {number} Annualized Risk
+//  */
+// function RISK_ANNUALIZED_WEEKLY(stdev) {
+//   return stdev * Math.sqrt(52);
+// }
 
-/**
- * Volatility Annualization - Monthly
- * @customfunction
- * @param {number} stdev Standard Deviation of the returns
- * @returns {number} Annualized Risk
- */
-function RISK_ANNUALIZED_MONTHLY(stdev) {
-  return stdev * Math.sqrt(12);
-}
+// /**
+//  * Volatility Annualization - Monthly
+//  * @customfunction
+//  * @param {number} stdev Standard Deviation of the returns
+//  * @returns {number} Annualized Risk
+//  */
+// function RISK_ANNUALIZED_MONTHLY(stdev) {
+//   return stdev * Math.sqrt(12);
+// }
 
-/**
- * Volatility Annualization - Quarterly
- * @customfunction
- * @param {number} stdev Standard Deviation of the returns
- * @returns {number} Annualized Risk
- */
-function RISK_ANNUALIZED_QUARTERLY(stdev) {
-  return stdev * Math.sqrt(4);
-}
+// /**
+//  * Volatility Annualization - Quarterly
+//  * @customfunction
+//  * @param {number} stdev Standard Deviation of the returns
+//  * @returns {number} Annualized Risk
+//  */
+// function RISK_ANNUALIZED_QUARTERLY(stdev) {
+//   return stdev * Math.sqrt(4);
+// }
 
-/**
- * Return Annualization - Daily
- * @customfunction
- * @param {number} rt Return
- * @returns {number} Annualized Return
- */
-function RETURN_ANNUALIZED_DAILY(rt) {
-  return rt * 252;
-}
+// /**
+//  * Return Annualization - Daily
+//  * @customfunction
+//  * @param {number} rt Return
+//  * @returns {number} Annualized Return
+//  */
+// function RETURN_ANNUALIZED_DAILY(rt) {
+//   return rt * 252;
+// }
 
-/**
- * Return Annualization - Weekly
- * @customfunction
- * @param {number} rt Return
- * @returns {number} Annualized Return
- */
-function RETURN_ANNUALIZED_WEEKLY(rt) {
-  return rt * 52;
-}
+// /**
+//  * Return Annualization - Weekly
+//  * @customfunction
+//  * @param {number} rt Return
+//  * @returns {number} Annualized Return
+//  */
+// function RETURN_ANNUALIZED_WEEKLY(rt) {
+//   return rt * 52;
+// }
 
-/**
- * Return Annualization - Monthly
- * @customfunction
- * @param {number} rt Return
- * @returns {number} Annualized Return
- */
-function RETURN_ANNUALIZED_MONTHLY(rt) {
-  return rt * 12;
-}
+// /**
+//  * Return Annualization - Monthly
+//  * @customfunction
+//  * @param {number} rt Return
+//  * @returns {number} Annualized Return
+//  */
+// function RETURN_ANNUALIZED_MONTHLY(rt) {
+//   return rt * 12;
+// }
 
-/**
- * Return Annualization - Quarterly
- * @customfunction
- * @param {number} rt Return
- * @returns {number} Annualized Return
- */
-function RETURN_ANNUALIZED_QUARTERLY(rt) {
-  return rt * 4;
-}
+// /**
+//  * Return Annualization - Quarterly
+//  * @customfunction
+//  * @param {number} rt Return
+//  * @returns {number} Annualized Return
+//  */
+// function RETURN_ANNUALIZED_QUARTERLY(rt) {
+//   return rt * 4;
+// }
 
-/**
- * Calculate the risk free rate
- * @customfunction
- * @param {number[][]} x
- * @returns {number} Risk Free Rate
- */
-function RISK_FREE_RATE(x) {
-  if (x.length > 1) {
-    const flattened = x.flat();
-    const sum = flattened.reduce(function (x, y) {
-      return x + y;
-    }, 0);
+// /**
+//  * Calculate the risk free rate
+//  * @customfunction
+//  * @param {number[][]} x
+//  * @returns {number} Risk Free Rate
+//  */
+// function RISK_FREE_RATE(x) {
+//   if (x.length > 1) {
+//     const flattened = x.flat();
+//     const sum = flattened.reduce(function (x, y) {
+//       return x + y;
+//     }, 0);
 
-    return sum / flattened.length / 100;
-  }
-  const sum = x[0].reduce(function (x, y) {
-    return x + y;
-  }, 0);
+//     return sum / flattened.length / 100;
+//   }
+//   const sum = x[0].reduce(function (x, y) {
+//     return x + y;
+//   }, 0);
 
-  return sum / x[0].length / 100;
-}
+//   return sum / x[0].length / 100;
+// }
 
-/**
- * Calculate the dirty price of the bond
- * @customfunction
- * @param {number} time_since_last_coupon time since last coupon (days/weeks/months)
- * @param {number} number_of_periods_between_coupon_paymnts number of periods between coupon (days/weeks/months)
- * @param {number} coupon_value Coupon Value
- * @param {number} clean_price Clean Price
- * @returns {number} Dirty Price
- */
-function DIRTY_PRICE(time_since_last_coupon, number_of_periods_between_coupon_paymnts, coupon_value, clean_price) {
-  const accrued_interest = (coupon_value * time_since_last_coupon) / number_of_periods_between_coupon_paymnts;
-  return accrued_interest + clean_price;
-}
+// /**
+//  * Calculate the dirty price of the bond
+//  * @customfunction
+//  * @param {number} time_since_last_coupon time since last coupon (days/weeks/months)
+//  * @param {number} number_of_periods_between_coupon_paymnts number of periods between coupon (days/weeks/months)
+//  * @param {number} coupon_value Coupon Value
+//  * @param {number} clean_price Clean Price
+//  * @returns {number} Dirty Price
+//  */
+// function DIRTY_PRICE(time_since_last_coupon, number_of_periods_between_coupon_paymnts, coupon_value, clean_price) {
+//   const accrued_interest = (coupon_value * time_since_last_coupon) / number_of_periods_between_coupon_paymnts;
+//   return accrued_interest + clean_price;
+// }
 
 // Real Estate Fianace Stuff
 /**
